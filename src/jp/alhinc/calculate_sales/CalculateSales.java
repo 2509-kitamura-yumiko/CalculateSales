63d794c3f58cd6066a740fd1320df78a9096d69b
kitamura.yumiko <kitamura.yumiko@alhinc.jp>
2025-12-02 16:18:48 +0900

    コードレビュー1回目の指摘事項の修正(52行目修正済)

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -49,7 +49,7 @@ public class CalculateSales {
 		List<File> rcdFiles = new ArrayList<>();
 
 		for(int i = 0; i < files.length; i++) {
-			if(files[i].getName().matches("^[0-9]{8}.rcd$")) {
+			if(files[i].getName().matches("^[0-9]{8}[.]rcd$")) {
 				rcdFiles.add(files[i]);
 			}
 		}
